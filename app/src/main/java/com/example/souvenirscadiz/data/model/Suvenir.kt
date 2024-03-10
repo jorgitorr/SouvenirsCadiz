37950df2d7c11c8cb8b4af0bdc35bb9713b29e9d
jorge <jorgearceruiz97@gmail.com>
2024-03-11 00:10:33 +0100

    Initial commit

---
+++ app/src/main/java/com/example/souvenirscadiz/data/model/Suvenir.kt
@@ -0,0 +1,7 @@
+package com.example.souvenirscadiz.data.model
+
+data class Suvenir(
+    val nombre:String,
+    val imagen:String,
+    val precio:Int
+)
---
+++ app/src/main/java/com/example/souvenirscadiz/data/model/User.kt
@@ -0,0 +1,5 @@
+package com.example.souvenirscadiz.data.model
+
+data class User(
+    var nombre:String,
+    var email:String)
