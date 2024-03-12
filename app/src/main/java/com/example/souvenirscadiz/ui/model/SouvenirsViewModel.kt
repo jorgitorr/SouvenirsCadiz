8a7475e60e0105816690d6415105ef391bf85b0a
jorge <jorgearceruiz97@gmail.com>
2024-03-12 20:48:50 +0100

    agregado lista de souvenirs

--- app/src/main/java/com/example/souvenirscadiz/ui/model/SouvenirsViewModel.kt
+++ app/src/main/java/com/example/souvenirscadiz/ui/model/SouvenirsViewModel.kt
@@ -2,6 +2,8 @@ package com.example.souvenirscadiz.ui.model
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.souvenirscadiz.data.model.Souvenir
+import com.example.souvenirscadiz.data.model.Tipo
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
@@ -11,8 +13,37 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
 class SouvenirsViewModel :ViewModel(){
+    /**
+     * @param query es una variable que se usa en el buscador para saber que estamos buscando
+     * @param active es una variable para saber si el buscador está activo
+     * @param _souvenirs es una variable privada  que contiene una lista de souvenirs
+     * @param souvenir es la variable que se comparte en el resto de páginas
+     * @param list lista que agrega cada souvenir
+     */
     val query = MutableStateFlow("")
     val active = MutableStateFlow(false)
+    private val _souvenirs = MutableStateFlow<List<Souvenir>>(emptyList())
+    val souvenirs = _souvenirs
+    init {
+        getSouvenirs()
+    }
+
+    /**
+     * obtiene todos los souvenirs
+     */
+    fun getSouvenirs(){
+        viewModelScope.launch {
+            val list: MutableList<Souvenir> = mutableListOf()
+            for(i in 4..18){
+                val souvenir = Souvenir()
+                souvenir.url = i
+                souvenir.tipo = Tipo.LLAVERO
+                list.add(souvenir)
+            }
+            _souvenirs.value = list
+        }
+    }
+
 
     /**
      * Actualiza la consulta de búsqueda actual.
