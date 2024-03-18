e1f1ad96fae5f1a1877b79787a7055d5c1d7013e
jorge <jorgearceruiz97@gmail.com>
2024-03-18 11:39:14 +0100

    al pulsar sobre la imagen te lleva a la página de detalle de esa imagen

--- app/src/main/java/com/example/souvenirscadiz/ui/view/PaginaPrincipal.kt
+++ app/src/main/java/com/example/souvenirscadiz/ui/view/PaginaPrincipal.kt
@@ -11,13 +11,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -29,6 +35,8 @@ import androidx.navigation.NavController
 import com.example.souvenirscadiz.ui.model.SouvenirsViewModel
 import com.example.souvenirscadiz.ui.theme.KiwiMaru
 import com.example.souvenirscadiz.ui.theme.KneWave
+import com.example.souvenirscadiz.ui.theme.RaisanBlack
+import com.example.souvenirscadiz.ui.theme.Redwood
 import com.example.souvenirscadiz.ui.theme.Silver
 
 @Composable
@@ -56,13 +64,15 @@ fun Principal(souvenirsViewModel: SouvenirsViewModel, navController: NavControll
 
 @Composable
 fun SouvenirDetail(navController: NavController, souvenirsViewModel: SouvenirsViewModel, nombre:String) {
+    var isFavorite by remember { mutableStateOf(false) }
     Scaffold(
         topBar = { Header(navController) },
         bottomBar = { Footer(navController, souvenirsViewModel) },
     ) { innerPadding ->
         Column(
             modifier = Modifier
-                .padding(innerPadding).background(Silver),
+                .padding(innerPadding)
+                .background(Silver),
             verticalArrangement = Arrangement.Bottom
         ) {
             val souvenir = souvenirsViewModel.getByReference(nombre)
@@ -71,6 +81,21 @@ fun SouvenirDetail(navController: NavController, souvenirsViewModel: SouvenirsVi
             Image(painter = painterResource(id = resourceId),
                 contentDescription = "")
             Text(text = souvenir.nombre, fontFamily = KiwiMaru)
+            Text(text = souvenir.referencia, fontFamily = KiwiMaru)
+            Text(text = souvenir.precio.toString()+"€", fontFamily = KiwiMaru)
+            
+            Text(text = "COMPRAR", fontFamily = KiwiMaru,
+                modifier = Modifier.clickable { })
+
+            Icon(
+                imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                contentDescription = "Favorite Icon",
+                tint = if (!isFavorite) RaisanBlack else Redwood,
+                modifier = Modifier
+                    .padding(vertical = 2.dp)
+                    .clickable { isFavorite = !isFavorite }
+            )
+            
         }
     }
 }
